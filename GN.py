c2ed570a15619b64dd803daa6d27a6da99d639fc
Vic Black <vic_black@163.com>
2019-05-15 15:17:41 +0800

    update GN

--- GN.py
+++ GN.py
@@ -20,7 +20,7 @@ class GroupNormalization(Layer):
     # Arguments
         groups: Integer, the number of groups for Group Normalization.
         axis: Integer, the axis that should be normalized
-            (typically the features axis).
+            (typically the features axis).moving_average
             For instance, after a `Conv2D` layer with
             `data_format="channels_first"`,
             set `axis=1` in `BatchNormalization`.
@@ -51,14 +51,14 @@ class GroupNormalization(Layer):
     def __init__(self,
                  groups=32,
                  axis=-1,
-                 momentum=0.99,
+                 # momentum=0.99,
                  epsilon=1e-5,
                  center=True,
                  scale=True,
                  beta_initializer='zeros',
                  gamma_initializer='ones',
-                 moving_mean_initializer='zeros',
-                 moving_variance_initializer='ones',
+                 # moving_mean_initializer='zeros',
+                 # moving_variance_initializer='ones',
                  beta_regularizer=None,
                  gamma_regularizer=None,
                  beta_constraint=None,
@@ -68,14 +68,14 @@ class GroupNormalization(Layer):
         self.supports_masking = True
         self.groups = groups
         self.axis = axis
-        self.momentum = momentum
+        # self.momentum = momentum
         self.epsilon = epsilon
         self.center = center
         self.scale = scale
         self.beta_initializer = initializers.get(beta_initializer)
         self.gamma_initializer = initializers.get(gamma_initializer)
-        self.moving_mean_initializer = initializers.get(moving_mean_initializer)
-        self.moving_variance_initializer = initializers.get(moving_variance_initializer)
+        # self.moving_mean_initializer = initializers.get(moving_mean_initializer)
+        # self.moving_variance_initializer = initializers.get(moving_variance_initializer)
         self.beta_regularizer = regularizers.get(beta_regularizer)
         self.gamma_regularizer = regularizers.get(gamma_regularizer)
         self.beta_constraint = constraints.get(beta_constraint)
@@ -83,21 +83,19 @@ class GroupNormalization(Layer):
 
     def build(self, input_shape):
         dim = input_shape[self.axis]
-
         if dim is None:
             raise ValueError('Axis ' + str(self.axis) + ' of '
-                                                        'input tensor should have a defined dimension '
-                                                        'but the layer received an input with shape ' +
+                             'input tensor should have a defined dimension '
+                             'but the layer received an input with shape ' +
                              str(input_shape) + '.')
-
         if dim < self.groups:
             raise ValueError('Number of groups (' + str(self.groups) + ') cannot be '
-                                                                       'more than the number of channels (' +
+                             'more than the number of channels (' +
                              str(dim) + ').')
 
         if dim % self.groups != 0:
             raise ValueError('Number of groups (' + str(self.groups) + ') must be a '
-                                                                       'multiple of the number of channels (' +
+                             'multiple of the number of channels (' +
                              str(dim) + ').')
 
         self.input_spec = InputSpec(ndim=len(input_shape),
@@ -125,21 +123,21 @@ class GroupNormalization(Layer):
         else:
             self.beta = None
 
-        self.moving_mean = self.add_weight(
-            shape=shape,
-            name="moving_mean",
-            initializer=self.moving_mean_initializer,
-            trainable=False)
-        self.moving_mean = K.reshape(self.moving_mean, broadcast_shape)
-        self.moving_mean = K.variable(value=self.moving_mean)
-
-        self.moving_variance = self.add_weight(
-            shape=shape,
-            name="moving_variance",
-            initializer=self.moving_variance_initializer,
-            trainable=False)
-        self.moving_variance = K.reshape(self.moving_variance, broadcast_shape)
-        self.moving_variance = K.variable(value=self.moving_variance)
+        # self.moving_mean = self.add_weight(
+        #     shape=shape,
+        #     name="moving_mean",
+        #     initializer=self.moving_mean_initializer,
+        #     trainable=False)
+        # self.moving_mean = K.reshape(self.moving_mean, broadcast_shape)
+        # self.moving_mean = K.variable(value=self.moving_mean)
+        #
+        # self.moving_variance = self.add_weight(
+        #     shape=shape,
+        #     name="moving_variance",
+        #     initializer=self.moving_variance_initializer,
+        #     trainable=False)
+        # self.moving_variance = K.reshape(self.moving_variance, broadcast_shape)
+        # self.moving_variance = K.variable(value=self.moving_variance)
 
         self.built = True
 
@@ -161,19 +159,21 @@ class GroupNormalization(Layer):
         gn_variance = K.var(inputs, axis=[2, 3, 4], keepdims=True)
 
         # compute group-normalization in different state
-        def gn_inference():
-            # when in test phase, just return moving_mean & moving_var
-            mean, variance = self.moving_mean, self.moving_variance
-            outputs = (inputs - mean) / (K.sqrt(variance + self.epsilon))
-            outputs = K.reshape(outputs, [-1, C, H, W]) * self.gamma + self.beta
-            # transpose: [bs,c,h,w] -> [ba,h,w,c]
-            if self.axis in {-1, 3}:
-                outputs = K.permute_dimensions(outputs, (0, 2, 3, 1))
-
-            return outputs
-
-        if training in {0, False}:
-            return gn_inference()
+        # def gn_inference():
+        #     # when in test phase, just return moving_mean & moving_var
+        #     # mean, variance = moving_mean, moving_variance
+        #     gn_mean = K.mean(inputs, axis=[2, 3, 4], keepdims=True)
+        #     gn_variance = K.var(inputs, axis=[2, 3, 4], keepdims=True)
+        #
+        #     outputs = (inputs - gn_mean) / (K.sqrt(gn_variance + self.epsilon))
+        #     outputs = K.reshape(outputs, [-1, C, H, W]) * self.gamma + self.beta
+        #     # transpose: [bs,c,h,w] -> [ba,h,w,c]
+        #     if self.axis in {-1, 3}:
+        #         outputs = K.permute_dimensions(outputs, (0, 2, 3, 1))
+        #     return outputs
+        #
+        # if training in {0, False}:
+        #     return gn_inference()
 
         outputs = (inputs - gn_mean) / (K.sqrt(gn_variance + self.epsilon))
         outputs = K.reshape(outputs, [-1, C, H, W]) * self.gamma + self.beta
@@ -182,35 +182,34 @@ class GroupNormalization(Layer):
         if self.axis in {-1, 3}:
             outputs = K.permute_dimensions(outputs, (0, 2, 3, 1))
 
-        self.add_update([K.moving_average_update(self.moving_mean,
-                                                 gn_mean,
-                                                 self.momentum),
-                         K.moving_average_update(self.moving_variance,
-                                                 gn_variance,
-                                                 self.momentum)],
-                        inputs)
+        # self.add_update([K.moving_average_update(self.moving_mean,
+        #                                          gn_mean,
+        #                                          self.momentum),
+        #                  K.moving_average_update(self.moving_variance,
+        #                                          gn_variance,
+        #                                          self.momentum)],
+        #                 inputs)
 
         # print("moving_mean shape : ",K.int_shape(self.moving_mean))
         # print("moving_mean: ",K.eval(self.moving_mean))
         # print("moving_variance shape: ",K.int_shape(self.moving_variance))
         # print("moving_variance: ",K.eval(self.moving_variance))
 
-        return K.in_train_phase(outputs,
-                                gn_inference,
-                                training=training)
+        # return K.in_train_phase(outputs, gn_inference, training=training)
+        return outputs
 
     def get_config(self):
         config = {
             'groups': self.groups,
             'axis': self.axis,
-            'momentum': self.momentum,
+            # 'momentum': self.momentum,
             'epsilon': self.epsilon,
             'center': self.center,
             'scale': self.scale,
             'beta_initializer': initializers.serialize(self.beta_initializer),
             'gamma_initializer': initializers.serialize(self.gamma_initializer),
-            'moving_mean_initializer': initializers.serialize(self.moving_mean_initializer),
-            'moving_variance_initializer': initializers.serialize(self.moving_variance_initializer),
+            # 'moving_mean_initializer': initializers.serialize(self.moving_mean_initializer),
+            # 'moving_variance_initializer': initializers.serialize(self.moving_variance_initializer),
             'beta_regularizer': regularizers.serialize(self.beta_regularizer),
             'gamma_regularizer': regularizers.serialize(self.gamma_regularizer),
             'beta_constraint': constraints.serialize(self.beta_constraint),
